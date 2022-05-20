9588a97777ac5faf1ce402e3c485374b1f7a1779
Bel-94 <belinda.ntinyari@student.moringaschool.com>
2022-05-20 15:07:59 +0300

    add all the final changes

--- app/config.py
+++ app/config.py
@@ -1,16 +1,32 @@
 import os
 
 class Config:
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://belinda:codes123@localhost/blogger'
+    UPLOADED_PHOTOS_DEST ='app/static/photos'
+    SESSION_COOKIE_SECURE = False
+    SECRET_KEY='Belminnyx'
 
-    pass
+    #  email configurations
+    MAIL_SERVER = 'smtp.googlemail.com'
+    MAIL_PORT = 587
+    MAIL_USE_TLS = True
+    MAIL_USERNAME = os.environ.get("MAIL_USERNAME")
+    MAIL_PASSWORD = os.environ.get("MAIL_PASSWORD")
+
+    WTF_CSRF_ENABLED = False
 
 
 class ProdConfig(Config):
-    pass
+    
+    SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL","")
+    if SQLALCHEMY_DATABASE_URI.startswith("postgres://"):
+        SQLALCHEMY_DATABASE_URI =SQLALCHEMY_DATABASE_URI.replace("postgres://","postgresql://",)
 
 
 class DevConfig(Config):
-    DEBUG = True
+    
+    DEBUG=True
 
 config_options = {
 'development':DevConfig,
--- start.sh
+++ start.sh
@@ -1,5 +1,5 @@
 export MAIL_USERNAME='codesrunner@gmail.com'
 export MAIL_PASSWORD='codes1234'
 export SECRET_KEY='Belminnyx'
-export DATABASE_URL='postgresql+psycopg2://belinda:2015@localhost/pitch'
+export DATABASE_URL='postgresql+psycopg2://belinda:codes123@localhost/blogger'
 python3.8 manage.py server
